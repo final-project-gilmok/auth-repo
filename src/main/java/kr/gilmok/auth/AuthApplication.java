9dfddf8a792ded995eaa8e951af48903975306c1
JunHyun <junhyun1001@gmail.com>
2026-02-10 11:41:25 +0900

    chore: base package 스캔 범위 지정
    #3

--- src/main/java/kr/gilmok/auth/AuthApplication.java
+++ src/main/java/kr/gilmok/auth/AuthApplication.java
@@ -2,10 +2,12 @@ package kr.gilmok.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ComponentScan(basePackages = {"kr.gilmok.auth", "kr.gilmok.common"})
 public class AuthApplication {
 
     public static void main(String[] args) {
