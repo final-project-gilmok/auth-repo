7ec2ec078cb076c3429eb561a795e667aba2cb4d
JunHyun <junhyun1001@gmail.com>
2026-02-11 17:20:00 +0900

    feat: 클래스명 변경 및 토큰 관련 에러 추가
    #5

--- src/main/java/kr/gilmok/auth/auth/exception/AuthErrorCode.java
+++ src/main/java/kr/gilmok/auth/auth/exception/AuthErrorCode.java
@@ -8,10 +8,19 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
+    /**
+     * 유저 관련 에러 (U)
+     */
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "U001", "이미 존재하는 아이디입니다."),
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "U002", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U003", "존재하지 않는 유저입니다."),
-    ;
+
+    /**
+     * 토큰 관련 에러 (T)
+     */
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "T001", "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "T002", "리프레시 토큰이 만료되었습니다. 다시 로그인해주세요."),
+    TOKEN_ALREADY_USED(HttpStatus.UNAUTHORIZED, "T003", "이미 사용된 토큰입니다. 보안을 위해 다시 로그인해주세요.");
 
     private final HttpStatus httpStatus;
     private final String code;
