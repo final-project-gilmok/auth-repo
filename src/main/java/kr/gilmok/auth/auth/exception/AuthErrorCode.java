b20a84af8d0972ce11bf5c0ba05a9efb634528a1
JunHyun <junhyun1001@gmail.com>
2026-02-11 17:00:20 +0900

    refactor: 클래스명 변경
    #5

--- src/main/java/kr/gilmok/auth/auth/exception/AuthErrorCode.java
+++ src/main/java/kr/gilmok/auth/auth/exception/AuthErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum UserErrorCode implements ErrorCode {
+public enum AuthErrorCode implements ErrorCode {
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "U001", "이미 존재하는 아이디입니다."),
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "U002", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U003", "존재하지 않는 유저입니다."),
