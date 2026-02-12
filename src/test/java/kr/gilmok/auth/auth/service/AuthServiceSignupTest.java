2f5489bf6b93fc73a5000d445806b9f45ea7e545
JunHyun <junhyun1001@gmail.com>
2026-02-12 16:19:10 +0900

    refactor: 클래스명 변경
    #5

--- src/test/java/kr/gilmok/auth/auth/service/AuthServiceSignupTest.java
+++ src/test/java/kr/gilmok/auth/auth/service/AuthServiceSignupTest.java
@@ -26,7 +26,7 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
-public class AuthServiceTest {
+public class AuthServiceSignupTest {
 
     @InjectMocks
     private AuthService authService;
