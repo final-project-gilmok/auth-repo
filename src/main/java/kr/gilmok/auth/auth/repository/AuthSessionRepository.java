0b5d232a8c9ca8116c1a9ff7033957fc6453fa9c
JunHyun <junhyun1001@gmail.com>
2026-02-24 10:15:20 +0900

    feat: 비관적 잠금 추가(동시성 문제)
    #5

--- src/main/java/kr/gilmok/auth/auth/repository/AuthSessionRepository.java
+++ src/main/java/kr/gilmok/auth/auth/repository/AuthSessionRepository.java
@@ -1,13 +1,16 @@
 package kr.gilmok.auth.auth.repository;
 
+import jakarta.persistence.LockModeType;
 import kr.gilmok.auth.auth.entity.AuthSession;
 import kr.gilmok.auth.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface AuthSessionRepository extends JpaRepository<AuthSession, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<AuthSession> findByRefreshTokenHashAndRevokedAtIsNull(String hash);
 
     List<AuthSession> findAllByUserAndRevokedAtIsNull(User user);
